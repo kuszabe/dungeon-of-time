48ea5894a9bbad901faa0d2d3e0a972e0093fc35
kuszabe <82150110+kuszabe@users.noreply.github.com>
2023-04-21 07:46:05 +0000

    rendering and damage fix

--- game.js
+++ game.js
@@ -1,121 +1,121 @@
 //initalizeee the game
-kaboom()
+kaboom({scale:3})
 
 //create variables
 const WOBBLESPEED = 1.5
 const MAXWOBBLE = 8
-const PLAYERSPEED = 100
+const PLAYERSPEED = 80
 
 //load the assets
 loadBean()
 
-loadRoot("/sprites")
+loadRoot("/sprites/")
 
-loadSprite("0","tile_0000")
-loadSprite("1","tile_0001")
-loadSprite("2","tile_0002")
-loadSprite("3","tile_0003")
-loadSprite("4","tile_0004")
-loadSprite("5","tile_0005")
-loadSprite("6","tile_0006")
-loadSprite("7","tile_0007")
-loadSprite("8","tile_0008")
-loadSprite("9","tile_0009")
-loadSprite("ö","tile_0010")
-loadSprite("ü","tile_0011")
-loadSprite("ó","tile_0012")
-loadSprite("q","tile_0013")
-loadSprite("w","tile_0014")
-loadSprite("e","tile_0015")
-loadSprite("r","tile_0016")
-loadSprite("t","tile_0017")
-loadSprite("z","tile_0018")
-loadSprite("u","tile_0019")
-loadSprite("i","tile_0020")
-loadSprite("o","tile_0021")
-loadSprite("p","tile_0022")
-loadSprite("ő","tile_0023")
-loadSprite("ú","tile_0024")
-loadSprite("ű","tile_0025")
-loadSprite("a","tile_0026")
-loadSprite("s","tile_0027")
-loadSprite("d","tile_0028")
-loadSprite("f","tile_0029")
-loadSprite("g","tile_0030")
-loadSprite("h","tile_0031")
-loadSprite("j","tile_0032")
-loadSprite("k","tile_0033")
-loadSprite("l","tile_0034")
-loadSprite("é","tile_0035")
-loadSprite("á","tile_0036")
-loadSprite("í","tile_0037")
-loadSprite("y","tile_0038")
-loadSprite("x","tile_0039")
-loadSprite("c","tile_0040")
-loadSprite("v","tile_0041")
-loadSprite("b","tile_0042")
-loadSprite("n","tile_0043")
-loadSprite("m","tile_0044")
-loadSprite(",","tile_0045")
-loadSprite(".","tile_0046")
-loadSprite("-","tile_0047")
-loadSprite("§","tile_0048")
-loadSprite("'","tile_0049")
-loadSprite("\"","tile_0050")
-loadSprite("+","tile_0051")
-loadSprite("!","tile_0052")
-loadSprite("%","tile_0053")
-loadSprite("/","tile_0054")
-loadSprite("=","tile_0055")
-loadSprite("(","tile_0056")
-loadSprite(")","tile_0057")
-loadSprite("Ö","tile_0058")
-loadSprite("Ü","tile_0059")
-loadSprite("Ó","tile_0060")
-loadSprite("Q","tile_0061")
-loadSprite("W","tile_0062")
-loadSprite("E","tile_0063")
-loadSprite("R","tile_0064")
-loadSprite("T","tile_0065")
-loadSprite("Z","tile_0066")
-loadSprite("U","tile_0067")
-loadSprite("I","tile_0068")
-loadSprite("O","tile_0069")
-loadSprite("P","tile_0070")
-loadSprite("Ő","tile_0071")
-loadSprite("Ú","tile_0072")
-loadSprite("Ű","tile_0073")
-loadSprite("A","tile_0074")
-loadSprite("S","tile_0075")
-loadSprite("D","tile_0076")
-loadSprite("F","tile_0077")
-loadSprite("G","tile_0078")
-loadSprite("H","tile_0079")
-loadSprite("J","tile_0080")
-loadSprite("K","tile_0081")
-loadSprite("L","tile_0082")
-loadSprite("É","tile_0083")
-loadSprite("Á","tile_0084")
-loadSprite("Í","tile_0085")
-loadSprite("Y","tile_0086")
-loadSprite("X","tile_0087")
-loadSprite("C","tile_0088")
-loadSprite("V","tile_0089")
-loadSprite("B","tile_0090")
-// loadSprite("N","tile_0091")
-// loadSprite("M","tile_0092")
-// loadSprite("M","tile_0093")
-// loadSprite("","tile_0094")
-// loadSprite("","tile_0095")
-// loadSprite("","tile_0096")
-// loadSprite("knight","tile_0097")
-// loadSprite("","tile_0098")
-// loadSprite("","tile_0099")
+loadSprite("0","tile_0000.png")
+loadSprite("1","tile_0001.png")
+loadSprite("2","tile_0002.png")
+loadSprite("3","tile_0003.png")
+loadSprite("4","tile_0004.png")
+loadSprite("5","tile_0005.png")
+loadSprite("6","tile_0006.png")
+loadSprite("7","tile_0007.png")
+loadSprite("8","tile_0008.png")
+loadSprite("9","tile_0009.png")
+loadSprite("ö","tile_0010.png")
+loadSprite("ü","tile_0011.png")
+loadSprite("ó","tile_0012.png")
+loadSprite("q","tile_0013.png")
+loadSprite("w","tile_0014.png")
+loadSprite("e","tile_0015.png")
+loadSprite("r","tile_0016.png")
+loadSprite("t","tile_0017.png")
+loadSprite("z","tile_0018.png")
+loadSprite("u","tile_0019.png")
+loadSprite("i","tile_0020.png")
+loadSprite("o","tile_0021.png")
+loadSprite("p","tile_0022.png")
+loadSprite("ő","tile_0023.png")
+loadSprite("ú","tile_0024.png")
+loadSprite("ű","tile_0025.png")
+loadSprite("a","tile_0026.png")
+loadSprite("s","tile_0027.png")
+loadSprite("d","tile_0028.png")
+loadSprite("f","tile_0029.png")
+loadSprite("g","tile_0030.png")
+loadSprite("h","tile_0031.png")
+loadSprite("j","tile_0032.png")
+loadSprite("k","tile_0033.png")
+loadSprite("l","tile_0034.png")
+loadSprite("é","tile_0035.png")
+loadSprite("á","tile_0036.png")
+loadSprite("í","tile_0037.png")
+loadSprite("y","tile_0038.png")
+loadSprite("x","tile_0039.png")
+loadSprite("c","tile_0040.png")
+loadSprite("v","tile_0041.png")
+loadSprite("b","tile_0042.png")
+loadSprite("n","tile_0043.png")
+loadSprite("m","tile_0044.png")
+loadSprite(",","tile_0045.png")
+loadSprite(".","tile_0046.png")
+loadSprite("-","tile_0047.png")
+loadSprite("§","tile_0048.png")
+loadSprite("'","tile_0049.png")
+loadSprite("\"","tile_0050.png")
+loadSprite("+","tile_0051.png")
+loadSprite("!","tile_0052.png")
+loadSprite("%","tile_0053.png")
+loadSprite("/","tile_0054.png")
+loadSprite("=","tile_0055.png")
+loadSprite("(","tile_0056.png")
+loadSprite(")","tile_0057.png")
+loadSprite("Ö","tile_0058.png")
+loadSprite("Ü","tile_0059.png")
+loadSprite("Ó","tile_0060.png")
+loadSprite("Q","tile_0061.png")
+loadSprite("W","tile_0062.png")
+loadSprite("E","tile_0063.png")
+loadSprite("R","tile_0064.png")
+loadSprite("T","tile_0065.png")
+loadSprite("Z","tile_0066.png")
+loadSprite("U","tile_0067.png")
+loadSprite("I","tile_0068.png")
+loadSprite("O","tile_0069.png")
+loadSprite("P","tile_0070.png")
+loadSprite("Ő","tile_0071.png")
+loadSprite("Ú","tile_0072.png")
+loadSprite("Ű","tile_0073.png")
+loadSprite("A","tile_0074.png")
+loadSprite("S","tile_0075.png")
+loadSprite("D","tile_0076.png")
+loadSprite("F","tile_0077.png")
+loadSprite("G","tile_0078.png")
+loadSprite("H","tile_0079.png")
+loadSprite("J","tile_0080.png")
+loadSprite("K","tile_0081.png")
+loadSprite("L","tile_0082.png")
+loadSprite("É","tile_0083.png")
+loadSprite("Á","tile_0084.png")
+loadSprite("Í","tile_0085.png")
+loadSprite("Y","tile_0086.png")
+loadSprite("X","tile_0087.png")
+loadSprite("C","tile_0088.png")
+loadSprite("V","tile_0089.png")
+loadSprite("B","tile_0090.png")
+// loadSprite("N","tile_0091.png")
+// loadSprite("M","tile_0092.png")
+// loadSprite("M","tile_0093.png")
+// loadSprite("","tile_0094.png")
+// loadSprite("","tile_0095.png")
+// loadSprite("","tile_0096.png")
+loadSprite("knight","tile_0097.png")
+loadSprite("sword","tile_0104.png")
+// loadSprite("","tile_0099.png")
 
 scene("game", () => {
     //make the player
     const player = add([
-        sprite("bean"),
+        sprite("knight"),
         area(),
         pos(center()),
         anchor("center"),
@@ -131,8 +131,8 @@ scene("game", () => {
 
     //make the sword
     const sword = player.add([
-        rect(10, 50),
-        pos(0, 0),
+        sprite("sword"),
+        pos(0, 5),
         anchor("bot"),
         area(),
         rotate(player.pos.angle(mousePos())),
@@ -146,33 +146,37 @@ scene("game", () => {
             if (sword.rotationoffset > 0) sword.rotationoffset *= -1
             sword.anchor = "top"
             if (sword.swing > 0) sword.swing *= -1
-            sword.pos.x = - 10
+            if (sword.swing > 0) sword.swing *= -1
+            sword.pos.x = -5
         } else {
             console.log("negatif")
             if (sword.rotationoffset < 0) sword.rotationoffset *= -1
             if (sword.swing < 0) sword.swing *= -1
-            sword.pos.x = 10
+            sword.pos.x = 5
             sword.anchor = "bot"
         }
         sword.rotateTo((player.pos.angle(mousePos())) + sword.rotationoffset)
     })
 
     //do damage to objects colliding if attacking
-    sword.onCollide("damageable", (obj) => {
-        if (sword.attack) obj.hurt(1)
+    sword.onCollideUpdate("damageable", (obj) => {
+        if (sword.attack && !obj.damaged) {
+            obj.hurt(1)
+            obj.damaged = true
+        }
     })
 
     loop(2, () => {
         add([
-            sprite("bean"),
-            color(BLUE),
+            sprite("C"),
             pos(rand() * 500, rand() * 500),
             goblin(0, 50, 1),
             entity(),
             area(),
             health(3),
             "enemy",
-            "damageable"
+            "damageable",
+            {damaged: false}
         ])
     })
 
@@ -218,6 +222,7 @@ scene("game", () => {
             wait(sword.duration, () => {
                 sword.attack = false
                 curTween.cancel()
+                get("damageable").forEach((obj) => obj.damaged = false)
             })
         })
     }
@@ -263,15 +268,15 @@ scene("game", () => {
                     destroy(this)
                 })
                 this.on("hurt", () => {
-                    let duration = 1
-                    let knockback = 22
+                    let duration = 0.7
+                    let knockback = 15
                     if (tweenCur) tweenCur.cancel()
                     tweenCur = tween(
                         this.pos,
                         this.pos.add((this.pos.sub(player.pos)).unit().scale(knockback)),
                         duration,
                         (val) => this.pos = val,
-                        easings.easeOutExpo
+                        easings.easeOutExpowa
                     )
                 })
             },
